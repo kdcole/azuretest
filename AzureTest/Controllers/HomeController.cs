cea2c882daa4e14beb109e48f5f45516e9b93840
Kevin Cole <kevin.cole@intimetec.com>
2018-07-09 22:24:08 -0700

    test

--- AzureTest/Controllers/HomeController.cs
+++ AzureTest/Controllers/HomeController.cs
@@ -11,7 +11,7 @@ namespace AzureTest.Controllers
         public ActionResult Index()
         {
             ViewBag.Title = "Home Page";
-
+            System.Diagnostics.Trace.TraceInformation("Testing");
             return View();
         }
     }
